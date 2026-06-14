c3906696615fa14dc4c10224669d33eaee07e6da
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-06-14 23:11:51 +0530

    feat(server): add logging functionality with Winston and Chalk

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,12 +1,3 @@
-import express from "express";
-
-const app = express();
-const port = 8080;
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-
-app.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
-});
+import "dotenv/config";
+import app from "./app";
+import { Server } from "http";
